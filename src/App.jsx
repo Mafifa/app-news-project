0c4d01149f0bd4a21a436198c3b770e96240bafc
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-02-23 19:51:23 -0400

    🦶Add the footer tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header'
 import { Container } from './components/Container'
 import { Body } from './components/Body'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Container>
         <Body />
       </Container>
+      <Footer />
 
     </>
   )
