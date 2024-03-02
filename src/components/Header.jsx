10d02e5be6f7a7cc98ff438ae0af135470cf4d9b
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-01 23:53:43 -0400

    Jus change the color background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className='mb-9 p-1 w-auto bg-[#fff3ec]'>
+    <header className='mb-9 p-1 w-auto bg-[#ffe3d3]'>
       <div className='pb-9 flex items-center justify-around lg:max-w-5xl mx-auto'>
         <div className='size-16'>
           <a href='https://mafifa.github.io/' target='_blank' rel='nooperner noreferrer' className='flex items-center text-[#2B2D42] font-bold'>
